dd7ecf8a83906295ba8ecfb8ab5c428505a9fd10
creaivecoder567 <sarathcreativethinker@gmail.com>
2018-01-31 11:28:13 +0530

    Svasth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                  dvsvs
+                Svasth
                 </div>
 
                 <div class="links">
