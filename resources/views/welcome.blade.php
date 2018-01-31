3862f174f2dd7836a3af3fe2284f996d7b3890c7
creaivecoder567 <sarathcreativethinker@gmail.com>
2018-01-31 11:24:13 +0530

    testing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                  dvsvs
                 </div>
 
                 <div class="links">
